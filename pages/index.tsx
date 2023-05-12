a8f155a9115db573c1d10ef6cf161348a19b93d3
harshittheone007 <harshittheone007@gmail.com>
2023-05-12 12:19:39 +0530

    Public layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const Home: NextPage = () => {
         setCookie("loggedinaspublic", "true", { expires: inTenHours });
 
         window.alert('Congrats, you have been logged in!!');
-        router.push("/fir");
+        router.push("/publicFir");
       } catch (error) {
         window.alert('Error connecting.');
       }
@@ -70,136 +70,12 @@ const Home: NextPage = () => {
 
   return (
     <div className="pl-2 font-poppins">
-      {/* <nav classNameName="flex items-center justify-between flex-wrap bg-teal-500 p-4">
-      <div classNameName="flex items-center flex-shrink-0 text-white mr-10">
-        <span classNameName="font-bold text-xl tracking-tight">DeFIR</span>
-      </div>
-      <div classNameName="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div classNameName="text-sm lg:flex-grow">
-          <a href="/fir" classNameName="block mt-4 lg:inline-block lg:mt-0 text-teal-50 hover:text-white mr-10">
-            FIR Registeration Portal
-          </a>
-          <a href="/evidence" classNameName="block mt-4 lg:inline-block lg:mt-0 text-teal-50 hover:text-white mr-10">
-            Evidence Storage
-          </a>
-          <a href="/firCards" classNameName="block mt-4 lg:inline-block lg:mt-0 text-teal-50 hover:text-white  mr-10">
-            FIR
-          </a>
-          <a href="/evidenceCards" classNameName="block mt-4 lg:inline-block lg:mt-0 text-teal-50 hover:text-white  mr-10">
-            Evidences
-          </a>
-        </div>
-        <Button onClick={requestAccount} size='lg' colorScheme='blue' mt='6px'>
-          Connect
-        </Button>
-      </div>
-    </nav>
-
-    <div classNameName="h-27 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">   
-    
-    <center>
-    <StatGroup>
-      <Stat>
-        <StatLabel>FIRs REGISTERED</StatLabel>
-        <StatNumber>345,670</StatNumber>
-        <StatHelpText>
-          <StatArrow type='increase' />
-          23.36%
-        </StatHelpText>
-      </Stat>
-
-      <Stat>
-        <StatLabel>Evidences Stored</StatLabel>
-        <StatNumber>45</StatNumber>
-        <StatHelpText>
-          <StatArrow type='decrease' />
-          9.05%
-        </StatHelpText>
-      </Stat>
-    </StatGroup>
-    </center>
-
-    
-      <main classNameName={styles.main}>
-
-      <Heading size='lg' fontSize='50px'>
-        Decentralized FIR 
-      </Heading>
-        <br/> */}
-
-      {/* <Link href="/fir">FIR REGISTERATION PORTAL</Link> */}
-      {/* <Link href="/evidence">Evidence Storage</Link> */}
-      {/* <Link href="/evidenceCards">Evidence Cards</Link> */}
-
-      {/* <Heading noOfLines={1}>
-          Address : {walletAddress}
-        </Heading>
-      </main>
-
-      <footer classNameName={styles.footer}>
-        <p>Decentralized FIR system - Main project</p>
-      </footer>
-    </div> */}
-      {/* <nav className="sm:px-4 py-2.5 opacity-100">
-        <div className="container flex px-16 flex-wrap items-center justify-between mx-auto">
-          <a href="/" className="flex items-center">
-            <span className="self-center text-xl font-bold whitespace-nowrap dark:text-black">
-              DFIR
-            </span>
-          </a>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="flex flex-row">
-              <li className="mt-2">
-                <a href="/fir" className="mx-5 my-5">
-                  FIR Registeration Portal
-                </a>
-              </li>
-              <li className="mt-2">
-                <a href="/dashboard" className="mx-5 my-5">
-                  Dashboard
-                </a>
-              </li>
-              <li className="mt-2">
-                <a href="/evidence" className="mx-5 my-5">
-                  Evidence storage
-                </a>
-              </li>
-              <li className="mt-2">
-                <a href="/firCards" className="mx-5 my-5">
-                  FIR
-                </a>
-              </li>
-              <li className="mt-2">
-                <a href="/evidenceCards" className="mx-5 my-5">
-                  Evidences
-                </a>
-              </li>
-              <li>
-                <button
-                  onClick={requestAccount}
-                  className="block py-2 pl-3 pr-4 text-black border-2 border-black translate-x-6"
-                >
-                  {walletAddress === ""
-                    ? "Connect Wallet"
-                    : "Logged in as " +
-                      walletAddress.substring(0, 8) +
-                      "..."}{" "}
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
       <div className="flex flex-wrap h-screen flex-row justify-between">
         <div className="flex flex-col w-6/12">
           <div className="flex flex-col justify-center px-20 gap-4">
          <div style ={{marginLeft:"-32px"}}>
                <img src="logo4.png" alt="logo" height="300px" width="300px" />
         </div>
-  
-
-
-
             <h1 className="text-7xl font-semibold text-[#131321] mt-1 leading-[90px]">
               <span className="font-black text-[#3661EB]">Decentralized</span> Crime Management System
             </h1>
